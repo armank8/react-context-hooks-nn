9e57b9e83acdacc360cc46ead810f4fa19b8ff95
Arman-Kabir <armankabir8@gmail.com>
2024-05-06 14:04:00 +0600

    context provider done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar></Navbar>
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/notes/2-5.js
@@ -0,0 +1,8 @@
+/**
+ * #2: Share state within a component tree
+ * 
+ * #3 : 
+ * 
+ * #4:0m
+ * 
+ */
